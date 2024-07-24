4ab929e1539208f306882519391260bdc56c6a35
AdrianaIngrid <150287447+AdrianaIngrid@users.noreply.github.com>
2024-07-24 15:37:34 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,87 +1,99 @@
 import React from 'react';
-import { Component } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import { nanoid } from 'nanoid';
 import ContactList from './ContactList/ContactList';
 import styles from './ContactForm/ContactForm.module.css';
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contacts: [
-        { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-        { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-        { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-        { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-      ],
-      filter: '',
-    };
-  }
+import { useState, useEffect } from 'react';
+function App() {
+  const [contacts, setContacts] = useState([
+    { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+    { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+    { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+    { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+    const [filter, setFilter] = useState('');
   // Aici incarcam contactele prima oara cand se da mount la aplicație
-  componentDidMount() {
-    console.log('ContactList mounted');
+  useEffect(() => {
+    console.log('Component did mount');
     const storedContacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(storedContacts);
-    if (parsedContacts) {
-      
-      this.setState({ storedContacts: parsedContacts });
- 
+       if (storedContacts) {
+      try {
+        const parsedContacts = JSON.parse(storedContacts);
+        console.log('Parsed contacts from localStorage:', parsedContacts);
+        if (Array.isArray(parsedContacts)) {
+          setContacts( parsedContacts);
+        }
+        
+      } catch (error){
+        console.error('Failed to parse contacts from localStorage:', error);
+      }
+     
     }
-  }
+   },[]);
+
   // Aici salvam contactele cand se face update la ele
-  componentDidUpdate(prevProps) {
-    if (prevProps.contacts !== this.state.contacts) {
-      console.log('ContactList updated');
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  useEffect(() => {
+    console.log('Component did update, saving contacts to localStorage');
+     localStorage.setItem('contacts', JSON.stringify(contacts));
+   }, [contacts]);
+ 
+
+  function handleNewContact(name, number) {
+    const newContact = { id: nanoid(), name, number };
+   const isDuplicate = contacts.some(contact =>
+     contact.name.toLowerCase() === name.toLowerCase()
+   );
+    if (isDuplicate) {
+      alert(`Contact with name ${name} is already in contacts.Please use a different name!`);
+      return;
     }
-  }
-
-  handleNewContact = ({ name, number }) => {
-    const newContact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-    this.state.contacts.some(
-      i => i.name.toLowerCase() === newContact.name.toLowerCase()
-    )
-      ? alert(`${name} is already in contacts`)
-      : this.setState(({ contacts }) => ({
-          contacts: [...contacts, newContact],
-        }));
+       setContacts( prevContacts  => 
+         [...prevContacts, newContact]
+      );
   };
-  handleFilterChange = ev => {
-    this.setState({ filter: ev.target.value });
+   const handleFilterChange = ev => {
+    setFilter( ev.target.value );
   };
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
-
-  render() {
-    const { contacts, filter } = this.state;
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
+  const deleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id),
     );
-    return (
-      <div>
-        <h1 className={styles.title}>Phonebook</h1>
-        <ContactForm handleNewContact={this.handleNewContact}></ContactForm>
+  };
 
-        <h2 className={styles.titleContact}>Contacts</h2>
-        <Filter
-          handleFilterChange={this.handleFilterChange}
-          filter={filter}
-        ></Filter>
-        <ContactList
-          contacts={filteredContacts}
-          deleteContact={this.deleteContact}
-        ></ContactList>
-      </div>
-    );
-  }
+  const filteredContacts = contacts.filter(contact => {
+    if (!contact) {
+      console.error("Invalid contact:", contact);
+      return false;
+    }
+    if (!contact.name) {
+      console.error("Contact with no name:", contact);
+      return false;
+    }
+    if (typeof contact.name !== 'string'){
+      console.log('Contact name is not a string.', contact)
+      return false;
+    }
+      
+    return contact.name.toLowerCase().includes(filter.toLowerCase());
+  });
+  
+   
+  return (
+    <div>
+      <h1 className={styles.title}>Phonebook</h1>
+      <ContactForm handleNewContact={handleNewContact}></ContactForm>
+
+      <h2 className={styles.titleContact}>Contacts</h2>
+      <Filter
+        handleFilterChange={handleFilterChange}
+        filter={filter}
+      ></Filter>
+      <ContactList
+        contacts={filteredContacts}
+        deleteContact={deleteContact}
+      ></ContactList>
+    </div>
+  );
 }
 
 export default App;
